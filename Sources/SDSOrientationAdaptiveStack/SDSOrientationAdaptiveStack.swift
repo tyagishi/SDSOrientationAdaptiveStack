123d54823be1807e06fea67d904193b395efb08d
Tomoaki Yagishita <tyagishi@gmail.com>
2021-06-03 15:36:29 +0900

    initial version

--- Sources/SDSOrientationAdaptiveStack/SDSOrientationAdaptiveStack.swift
+++ Sources/SDSOrientationAdaptiveStack/SDSOrientationAdaptiveStack.swift
@@ -1,3 +1,40 @@
-struct SDSOrientationAdaptiveStack {
-    var text = "Hello, World!"
+//
+//  SDSOrientationAdaptiveStack.swift
+//
+//  Created by : Tomoaki Yagishita on 2021/06/03
+//  © 2021  SmallDeskSoftware
+//
+
+import SwiftUI
+import Combine
+
+public struct SDSOrientationAdaptiveStack<Content1: View, Content2: View> : View {
+    var firstContent: Content1
+    var secondContent: Content2
+    @State var orientation: UIDeviceOrientation
+    
+    public init(@ViewBuilder first: () -> Content1, @ViewBuilder second: () -> Content2 ) {
+        self.firstContent = first()
+        self.secondContent = second()
+        self._orientation = State(wrappedValue: UIDevice.current.orientation)
+    }
+
+    public var body: some View {
+        ZStack {
+            if orientation == .landscapeRight || orientation == .landscapeLeft {
+                HStack {
+                    firstContent
+                    secondContent
+                }
+            } else {
+                VStack {
+                    firstContent
+                    secondContent
+                }
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+            orientation = UIDevice.current.orientation
+        }
+    }
 }
--- Tests/SDSOrientationAdaptiveStackTests/SDSOrientationAdaptiveStackTests.swift
+++ Tests/SDSOrientationAdaptiveStackTests/SDSOrientationAdaptiveStackTests.swift
@@ -6,6 +6,7 @@
             // This is an example of a functional test case.
             // Use XCTAssert and related functions to verify your tests produce the correct
             // results.
-            XCTAssertEqual(SDSOrientationAdaptiveStack().text, "Hello, World!")
+            //XCTAssertEqual(SDSOrientationAdaptiveStack().text, "Hello, World!")
+            XCTFail("No tests!")
         }
     }
